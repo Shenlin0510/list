8086982ce726df137bda21f75d74dbcb19de34ec
shen lin <chrislin0510@gmail.com>
2022-04-09 14:36:37 +0800

    updates

--- 2list.py
+++ 2list.py
@@ -1,10 +1,18 @@
 products=[]
+with open("products.csv",mode="r",encoding="utf-8") as f:
+    for line in f:
+        if "商品,價格" in line:
+            continue
+        name,price=line.strip().split(",")
+        products.append([name,price])
+
 while True:
     name=input("請輸入商品名：")
     if name== "q":
         break
     n=[]
     price=input("請輸入價格：")
+    price=int(price)
     n.append(name)
     n.append(price)
     products.append(n)
@@ -16,4 +24,4 @@ for p in products:
 
 with open ("products.csv" , mode="w") as f:
     for p in products:
-        f.write(p[0]+","+p[1]+"\n")
+        f.write(p[0]+","+str(p[1])+"\n")
